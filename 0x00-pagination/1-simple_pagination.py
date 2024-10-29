5219c36f0b1a17a7d47ff2bb743d066df5ca3182
Kelmfaker <kelmfaker@gmail.com>
2024-10-29 21:48:47 +0000

    simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -15,9 +15,9 @@ def index_range(page, page_size):
     Returns:
         tuple: A tuple containing the start index and the end index.
     """
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return start_index, end_index
+    start = (page - 1) * page_size
+    end = start + page_size
+    return start, end
 
 
 class Server:
