49e47121ad9eb99f07a665c8afee1ce07520524e
Kelmfaker <kelmfaker@gmail.com>
2024-10-29 19:59:11 +0000

    simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -40,6 +40,9 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        method takes two integer with default values
+        """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
         start_index, end_index = index_range(page, page_size)
