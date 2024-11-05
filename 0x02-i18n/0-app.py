1c848378dc6e9642128fe2d4fc5740974fe01112
Kelmfaker <kelmfaker@gmail.com>
2024-11-05 09:45:48 +0000

    basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python3
-
-"""
-setup a basic Flask app
-"""
-
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
 @app.route('/')
 """
 function returns render template
