e43d5dfac191817418ea6301f6e25f1428374d5a
Kelmfaker <kelmfaker@gmail.com>
2024-11-06 09:56:53 +0000

    URL Parameter

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -15,7 +15,7 @@ class Config:
     Configuration class for Flask app.
     """
     LANGUAGES = ['en', 'fr']
-    BABEL_DEFAULT_LOCALE = 'fr'
+    BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
